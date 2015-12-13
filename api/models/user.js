90e3484cccf0683d92da832805904009b2060214
CharlieAllen <miss.neophyte@gmail.com>
2015-12-13 10:58:35 +0000

    user model added

---
+++ api/models/user.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var UserSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true},
+  password: { type: String },
+  location: { type: String },
+  industry: { type: String },
+  about_me: { type: String }
+});
+
+UserSchema.set('toJSON', {
+  transform: function(doc, ret, options) {
+    var returnJson = {
+      id: ret._id,
+      name: ret.name,
+      location: ret.location,
+      industry: ret.industry,
+      about_me: ret.about_me
+    };
+    return returnJson;
+  }
+});
+
+UserSchema.pre('save', function(next) {
+  this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  next();
+});
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model("User", UserSchema);
